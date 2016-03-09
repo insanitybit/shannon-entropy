891b9bd702855d5b50e6743a5f61cc290c488a5a
insanitybit <insanitybit@gmail.com>
2016-03-08 21:50:59 -0500

    Benchmarks. Move division. Handle non-ASCII length strings.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Add this to your Cargo.toml
 
 ```toml
 [dependencies]
-shannon-entropy = "0.1"
+shannon-entropy = "0.2"
 ```
 
 # Example
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(test)]
 use std::collections::BTreeMap;
 use std::ascii::AsciiExt;
+
 /// Calculates the shannon entropy of 's'.
 /// https://en.wiktionary.org/wiki/Shannon_entropy
 ///
@@ -10,7 +11,6 @@ use std::ascii::AsciiExt;
 ///
 /// # Example
 /// let entropy : f32 = shannon_entropy("Hi there!");
-
 pub fn shannon_entropy(s: &str) -> f32 {
     if s.is_empty() {
         return 0.0
@@ -19,31 +19,37 @@ pub fn shannon_entropy(s: &str) -> f32 {
 
     let mut ascii_map: [usize; 128] = [0;128];
 
+    let mut s_len = 0;
+
     for ch in s.chars() {
+        s_len += 1;
         if ch.is_ascii() {
             ascii_map[ch as usize] += 1;
         } else {
             *char_map.entry(ch).or_insert(0) += 1;
         }
     }
-    let s_len = (s.len() as f32).round();
+
+    let s_len = (s_len as f32).round();
     let log_div = (2.0 as f32).ln();
 
     let result = char_map.values().fold(0.0, |acc, c| {
         if * c == 0 {
-            acc + 0.0
+            acc
         } else {
-            acc + (*c as f32 * (*c as f32 / s_len).ln())/(s_len * log_div)
+            acc + (*c as f32 * (*c as f32 / s_len).ln())
         }
     });
 
-    ascii_map.into_iter().fold(result, |acc, c| {
+    let result = ascii_map.into_iter().fold(result, |acc, c| {
         if * c == 0 {
-            acc + 0.0
+            acc
         } else {
-            acc + (*c as f32 * (*c as f32 / s_len).ln())/(s_len * log_div)
+            acc + (*c as f32 * (*c as f32 / s_len).ln())
         }
-    }).abs()
+    }).abs();
+    let result = result / (s_len * log_div);
+    result
 }
 
 #[cfg(test)]
@@ -59,10 +65,16 @@ mod tests {
     fn test_shannon() {
 
         let test_strings = vec![
-                                ("hello world", 2.8453512),
+                                // All-ASCII strings hit the fast path
+                                ("hello world", 2.845351),
                                 ("hello worldd", 2.8553884),
                                 ("a", 0.0),
                                 ("", 0.0),
+                                // // Test non-ascii characters for slow path
+                                ("i ❤ rust", 2.7499998),
+                                ("ßℝ💣", 1.5849625),
+                                ("abc", 1.5849625),
+                                ("hello world💣", 3.0220554),
                                 ];
         for (test, answer) in test_strings {
             let entropy = shannon_entropy(test);
@@ -70,7 +82,6 @@ mod tests {
         }
 
     }
-
 
     #[bench]
     fn bench_shannon_empty(b: &mut Bencher) {
@@ -80,21 +91,18 @@ mod tests {
     #[bench]
     fn bench_shannon_small(b: &mut Bencher) {
         let s: String = thread_rng().gen_ascii_chars().take(64).collect();
-
         b.iter(|| shannon_entropy(&s));
     }
 
     #[bench]
     fn bench_shannon_medium(b: &mut Bencher) {
         let s: String = thread_rng().gen_ascii_chars().take(1024).collect();
-
         b.iter(|| shannon_entropy(&s));
     }
 
     #[bench]
     fn bench_shannon_large(b: &mut Bencher) {
         let s: String = thread_rng().gen_ascii_chars().take(65536).collect();
-
         b.iter(|| shannon_entropy(&s));
     }
 }
